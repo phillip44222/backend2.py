eff73c2b5ee4e9e3da841c2993cdaab35592f215
phillip44222 <1531866427@qq.com>
2023-09-24 13:33:21 +0800

    Changes

---
+++ SE-PROJ-Back-end/backend.py
@@ -0,0 +1,9 @@
+class User_id:
+
+    def __init__(self, username, phone_number, email, credit_card_numbers):
+
+        self.username = username
+        self.phone_number = phone_number
+        self.email = email
+        self.credit_card_numbers = credit_card_numbers
+    
